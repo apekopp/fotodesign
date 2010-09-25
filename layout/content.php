d711d74bd731199e8316c4305d750b3dd59c00b2
Your Name <apekopp@gmx.de>
2010-09-25 15:59:51 +0200

    new height

--- layout/content.php
+++ layout/content.php
@@ -3,4 +3,3 @@
 
 	Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
 </div>
-
--- layout/navi.php
+++ layout/navi.php
@@ -1,18 +1,18 @@
 <div id="navi">
 	<ul class="dropdown dropdown-upward">
-		<li><a href="./">Home</a></li>
+		<li><a href="">Home</a></li>
 		<li><span>K2OP</span></li>
 		<li><span>Galerie</span>
 			<ul>
-				<li><a href="./">Portrait</a></li>
-				<li><a href="./">Hochzeiten</a></li>
-				<li><a href="./">Natur</a></li>
-				<li><a href="./">Stilleben</a></li>
-				<li><a href="./">Architektur</a></li>
+				<li><a href="">Portrait</a></li>
+				<li><a href="">Hochzeiten</a></li>
+				<li><a href="">Natur</a></li>
+				<li><a href="">Stilleben</a></li>
+				<li><a href="">Architektur</a></li>
 			</ul>
 		</li>
 		<li><span><a href="./kontakt.php">Kontakt</a></span>
 		</li>
-		<li><a href="./">Impressum</a></li>
+		<li><a href="">Impressum</a></li>
 	</ul>
 </div>
